3ec873f0b3389f789692682949368e4f56adcd1e
Michael B <dev@mihael.me>
2016-02-10 16:59:08 +0200

    implement user session

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -2,12 +2,12 @@
 
 class IndexController extends BaseController {
     public function execute($arguments = []) {
+
         require_once 'views/parts/header.php';
 
         require_once 'views/main.php';
 
         require_once 'views/parts/footer.php';
-
 
         return true;
     }
